a292d9c7cafc17f2298a6f2bdfb42cb8d10fdea5
Zack Stout <zackstout@gmail.com>
2018-07-18 15:30:03 -0500

    i think it might be working...

--- client.js
+++ client.js
@@ -1,7 +1,6 @@
 
 const endpoints = [{x: 1, y: 1}, {x: 0, y: 3}];
-
-const connectors = [[{x: 1, y: 1}, {x: 1, y: 2}], [{x: 1, y: 2}, {x: 1, y: 3}], [{x: 1, y: 3}, {x: 0, y: 3}]];
+const connectors = [[{x: 1, y: 1}, {x: 1, y: 2}], [{x: 1, y: 2}, {x: 1, y: 3}], [{x: 1, y: 3}, {x: 0, y: 3}], [{x: 1, y: 0}, {x: 1, y: 1}], [{x: 0, y: 0}, {x: 1, y: 0}], [{x: 1, y: 1}, {x: 2, y: 1}]];
 
 // ===============================================================================================
 
@@ -22,6 +21,7 @@ function getStandardEdge(edge) {
       edge[1].x = temp;
     }
   }
+  return edge;
 }
 
 // ===============================================================================================
@@ -34,6 +34,8 @@ function getOtherPoint(point, edge) {
 // ===============================================================================================
 
 function edgeInArray(e, arr) {
+  // console.log(e, arr);
+  e = getStandardEdge(e);
   if (e.length < 1) return false;
   for (let i=0; i < arr.length; i++) {
     if (arr[i][0].x == e[0].x && arr[i][0].y == e[0].y && arr[i][1].x == e[1].x && arr[i][1].y == e[1].y) return true;
@@ -43,53 +45,75 @@ function edgeInArray(e, arr) {
 
 // ===============================================================================================
 
-function computeVertices(cell) {
-  const UL = {pos: 'UL', x: cell.x    , y: cell.y    };
-  const UR = {pos: 'UR', x: cell.x + 1, y: cell.y    };
-  const BL = {pos: 'BL', x: cell.x    , y: cell.y + 1};
-  const BR = {pos: 'BR', x: cell.x + 1, y: cell.y + 1};
-  const vertices = [UL, UR, BL, BR];
-  return vertices;
+// I see the problem: this is to find a CELL's edges, not the edges adjacent to a VERTEX -- THAT'S why there are always four of them!
+function computeEdges(vtx) {
+  // Just leave the negatives for now:
+  return [
+    [{x: vtx.x - 1, y: vtx.y}, {x: vtx.x, y: vtx.y}],
+    [{x: vtx.x, y: vtx.y}, {x: vtx.x + 1, y: vtx.y}],
+    [{x: vtx.x, y: vtx.y - 1}, {x: vtx.x, y: vtx.y}],
+    [{x: vtx.x, y: vtx.y}, {x: vtx.x, y: vtx.y + 1}],
+  ];
 }
 
 // ===============================================================================================
 
-function computeEdges(cell) {
-  const vertices = computeVertices(cell);
-  const top    = [vertices[0], vertices[1]];
-  const right  = [vertices[1], vertices[3]];
-  const bottom = [vertices[2], vertices[3]];
-  const left   = [vertices[0], vertices[2]];
-  const edges   = [top, right, bottom, left];
-  return edges;
-}
+function areConnected(v1, v2, occ_edges, path=[], checked=[]) {
+  occ_edges = occ_edges.map(e => getStandardEdge(e));
+  // console.log(checked);
 
-// ===============================================================================================
+  // Exit condition:
+  const edges1 = computeEdges(v1);
+  const edges2 = computeEdges(v2);
+  edges1.forEach(e => {
+    edges2.forEach(e2 => {
+      if (edgeInArray(e, checked) && edgeInArray(e2, checked)) {
+        console.log('yeaaa');
+        return true;
+      }
+    });
+  });
 
-function areConnected(v1, v2, occ_edges, path=[]) {
-  let currentVtx = v1;
-  const edges = computeEdges(currentVtx);
+  // Dead end:
+  while (checkForImportantEdges(v1, occ_edges, checked).length == 0) {
+    // console.log(path);
+    const prevEdge = path.pop();
+    // console.log(path);
+    // console.log(prevEdge);
+    const prevPoint = getOtherPoint(v1, prevEdge);
+    v1 = prevPoint;
+    // console.log(v1);
+    if (path.length == 0) {
+      console.log('aha');
+      // return false;
+    }
+  }
 
-  const important_edges = edges.filter(e => edgeInArray(e, occ_edges));
+  const important_edges = checkForImportantEdges(v1, occ_edges, checked);
 
   important_edges.forEach(edge => {
-    // Will this work?
-    currentVtx = getOtherPoint(currentVtx, edge);
+    const other = getOtherPoint(v1, edge);
+    edge = getStandardEdge(edge);
 
-    // NO! We should recurse here!!!
-    const more_edges = computeEdges(currentVtx);
-    more_edges.forEach(edge2 => {
-      if (!edgeInArray(edge2, path)) {
+    // Two arrays: one tracks current path, one tracks all visited edges.
+    path.push(edge);
+    checked.push(edge);
 
-      }
-
-    });
+    areConnected(other, v2, occ_edges, path, checked);
   });
 
 }
 
 // ===============================================================================================
 
-areConnected(endpoints[0], endpoints[1], connectors);
+function checkForImportantEdges(vtx, occ_edges, checked) {
+  // console.log(vtx);
+  // console.log(computeEdges(vtx).filter(e => edgeInArray(e, occ_edges)));
+  return computeEdges(vtx).filter(e => edgeInArray(e, occ_edges)).filter(e => !edgeInArray(e, checked));
+}
 
-console.log(endpoints, connectors);
+// ===============================================================================================
+
+const res = areConnected(endpoints[0], endpoints[1], connectors);
+
+console.log(res, endpoints, connectors);
